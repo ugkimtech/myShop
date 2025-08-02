a6b4215c9d38af0399effc497385cee7089e8f57
charleskim474 <charleskim474@gmail.com>
2025-08-02 08:57:40 +0300

    static files into templates

--- Users/logic/expenditure.py
+++ Users/logic/expenditure.py
@@ -5,9 +5,10 @@ class ExpenditureManager:
     def expend(self, amount, reason):
         Expenditure.objects.create(amount=amount, reason=reason)
         status = Status.objects.get(id=1)
-        cash = status.available_cash - amount
+        cash = float(status.available_cash) - amount
         status.available_cash = cash
-        dev = status.development - amount
+        dev = float(status.development) - amount
         status.development = dev
+        status.weight = cash + float(status.stock_amount)
         status.save()
         return
